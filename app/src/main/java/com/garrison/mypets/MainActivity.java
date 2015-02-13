eff73e848e23d87079c52b9f0cd9b181c1a2d5a3
Garrison Wright <wrightnow99@gmail.com>
2015-02-12 22:47:23 -0600

    Removed data sync

--- app/src/main/java/com/garrison/mypets/MainActivity.java
+++ app/src/main/java/com/garrison/mypets/MainActivity.java
@@ -5,8 +5,6 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 
 import com.garrison.mypets.services.ContactsNotificationService;
-import com.garrison.mypets.sync.VetFinderSyncAdapter;
-import com.garrison.mypets.util.LocationFinder;
 
 
 public class MainActivity extends ActionBarActivity implements MainFragment.Callback{
@@ -21,12 +19,8 @@ public class MainActivity extends ActionBarActivity implements MainFragment.Call
         Intent mServiceIntent = new Intent(this, ContactsNotificationService.class);
         startService(mServiceIntent);
 
-        String locationString = LocationFinder.getLocationLongLatString(this);
-        VetFinderSyncAdapter.setLocationString(locationString);
-        VetFinderSyncAdapter.syncImmediately(this);
-
         MainFragment mainPetsFragment = (MainFragment) getSupportFragmentManager()
-        .findFragmentById(R.id.fragment_main);
+            .findFragmentById(R.id.fragment_main);
     }
 
     // Implementation of Call back from MainFragment
